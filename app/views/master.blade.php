74d17e12c29ec859598345800d8b929b941d8d33
johndavedecano <johndavedecano@gmail.com>
2013-10-24 00:37:56 +0800

    Added

---
+++ app/views/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Poker Membership</title>
+		<link rel="stylesheet" href="{{ URL::to('assets/css/uikit.min.css') }}" />
+		<script src="{{ URL::to('assets/js/jquery.js') }}"></script>
+		<script src="{{ URL::to('assets/js/uikit.min.js') }}"></script>
+	</head>
+	<body>
+        @yield('content')
+	</body>
+</html>
